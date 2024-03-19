9750088f77db504740a0970e9e1315b31c30cf21
DEVang0876 <23aiml014@charusat.edu.in>
2024-03-19 20:50:40 +0530

    complex

--- oop7.cpp
+++ oop7.cpp
@@ -1,17 +1,99 @@
-//devang dhandhukiya
-//OOPs 7    23AIML014 dev
 #include<iostream>
 using namespace std;
-int main();
-class cox
+
+class complex
 {
     float real,img;
-    public:
+    static int count;
+public:
     void printcox();
-    void addcox();
+    complex(); 
+    complex(float a, float b); 
+    complex operator+(const complex &c) const
+    {
+        complex temp;
+        temp.img = img + c.img;
+        temp.real = real + c.real;
+        return temp;
+    }
+    complex operator-(const complex &c) const
+    {
+        complex temp;
+        temp.img = img - c.img;
+        temp.real =real - c.real;
+        return temp;
+    }
+    complex operator*(const complex &c) const
+    {
+        complex temp;
+        temp.img =(img)*(c.img);
+        temp.real = (real) * (c.real);
+        return temp;
+    }
+    complex operator/(const complex &c) const
+    {
+        complex temp;
+        temp.img =(img)/(c.img);
+        temp.real = (real) / (c.real);
+        return temp;
+    }
 
 };
+int complex:: count=1;
+complex::complex()
+{
+    float a,b;
+    cout<<endl<<"Enter Real value for Complex "<<count<<" :";
+    cin>>a;
+    cout<<endl<<"Enter Imaginary value for Complex "<<count<<" :";
+    cin>>b;
+    img=b;real=a;
+    count++;
+}
+
+complex::complex(float a, float b)
+{
+    img = b;
+    real = a;
+    count++;
+}
+
+void complex::printcox()
+{
+    cout<<endl<<real<<" + "<<img<<"i";
+}
+
 int main()
 {
+    complex c3 = complex(0,0);
+    complex c2,c1;
+    cout<<endl<<"Choose option";
+    cout<<endl<<"(+)  AAddition";
+    cout<<endl<<"(-)  Substraction";
+    cout<<endl<<"(*)  Multiplication";
+    cout<<endl<<"(/)  Devision";
+    char ch; cout<<endl;
+    cin>>ch;
+    switch (ch)
+    {
+    case 43:
+        c3=c1+c2;
+        break;
+    case 42:
+        c3=c1-c2;
+        break;
+    case 45:
+        c3=c1*c2;
+        break;
+    case 47:
+        c3=c1/c2;
+        break;
+    
+    default:
+        break;
+    }
+    c1.printcox();
+    c2.printcox();
+    c3.printcox();
     return 0;
 }
