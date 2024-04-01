f021f1148f9197ee14e60f038ef44986712cd711
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-01 18:21:29 +0530

    prac 9 DONe

--- oop9.cpp
+++ oop9.cpp
@@ -1,40 +1,47 @@
 #include<iostream>
 using namespace std;
 int main();
+class MM;
 class CM
 {
     float cm;
     public:
-    void inputcm();
+    void inputcm(){cin>>cm;}
     float getcm();
-    
-
+    void putcm(){cout<<cm;}
+    CM(){cm=0;}
+    CM(MM &y);
 };
 class MM
 {
     float mm;
     public:
-    float getmm();
     MM(){mm=0;}
     MM(CM &x)
     {
-        float temp;
-        temp=x.getcm();
-        cout<<(100*(temp));
+        float tempp=x.getcm();
+        mm=(100*(tempp));
     }
+    float getmm(){return mm;}
+    void inputmm();
+    void putmm(){cout<<mm;}
 };
+CM:: CM(MM &y)
+{
+    float temp=y.getmm();
+    cm=(temp/100);
+}
+void MM::inputmm()
+{
+    cin>>mm;
+}
 float CM :: getcm()
 {
     return cm;
 }
-float MM:: getmm()
-{
-    cout<<mm;
-}
 int main()
 {
-    CM a;
-    MM b;
+    CM a; MM b;
     menu:
     int i;
     cout<<endl<<"Enter 1 : mm to cm converter";
@@ -42,24 +49,21 @@ int main()
     cin>>i;
     if(i==1)
     {
-        int j;
-        cout<<endl<<"Enter measurement in mm";
-        cin>>j;
-        cout<<endl<<"Measurement in cm : "<<(j/(100));
-
+        cout<<endl<<"Enter measurement in mm : ";
+        b.inputmm();
+        a=b;
+        cout<<endl<<"Measurement in cm : ";a.putcm();
     }
     else if(i==2)
-    {
-        int k;
-        MM temp;
-        cout<<endl<<"Enter measurement in mm";
-        cin>>k;
-        cout<<endl<<"Measurement in mm : ";
+    {  
+        cout<<endl<<"Enter measurement in cm : ";
+        a.inputcm();
+        b=a;
+        cout<<endl<<"Measurement in mm : "; b.putmm();
     }
     else{
         cout<<endl<<"INVALID INPUT !!!!";
         goto menu;
     }
-
     return 0;
 }
