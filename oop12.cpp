87b383ecca3f5b02b7134b59062ff9de2d74ca0a
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-08 18:09:48 +0530

    ops

--- oop12.cpp
+++ oop12.cpp
@@ -29,8 +29,10 @@ class Result
 void Student :: getdata_stu()
 {
     cout<<endl<<"ENTER STUDENT ID: ";
+    cin.ignore();
     cin.getline(stu_id,7);
     cout<<endl<<"ENTER STUDENT NAME: ";
+    cin.ignore();
     cin>>stu_name;
     cout<<endl<<"ENTER SEMESTER: ";
     cin>>sem;
@@ -46,8 +48,10 @@ void Student:: getdata_result(Result &ob)
     for(int i=0; i<3; i++)
     {
     cout<<endl<<"Enter Subject "<<i<<" Name: ";
+    cin.ignore();
     cin>>ob.subject_name[i];
     cout<<endl<<"Enter Subject "<<i<<" Credit";
+    cin.ignore();
     cin>>ob.subject_credit[i];
     cout<<endl<<"Enter Theory Marks: ";
     cin>>ob.th_mark[i];
@@ -57,7 +61,7 @@ void Student:: getdata_result(Result &ob)
 }
 void Student :: putdata_result(Result &ob)
 {
-    int grade_point[3],mul_cr_gd=0;
+    int grade_point[6],mul_cr_gd=0;
 
     for(int i=0; i<3; i++)
     {
@@ -102,6 +106,50 @@ void Student :: putdata_result(Result &ob)
             grade_point[i]=0;
         }
         mul_cr_gd+=(grade_point[i]*ob.subject_credit[i]);
+    }
+    for(int i=0; i<3; i++)
+    {
+        if(ob.pr_mark[i]>=80)
+        {
+           ob.pr_grade[i]="AA";
+           grade_point[i+3]=10;
+        }
+        else if(ob.pr_mark[i]<80 && ob.pr_mark[i]>=73)
+        {
+            ob.pr_grade[i]="AB";
+            grade_point[i+3]=9;
+        }
+        else if(ob.pr_mark[i]<73 && ob.pr_mark[i]>=66)
+        {
+            ob.pr_grade[i]="BB";
+            grade_point[i+3]=8;
+        }
+        else if(ob.pr_mark[i]<66 && ob.pr_mark[i]>=60)
+        {
+            ob.pr_grade[i]="BC";
+            grade_point[i+3]=7;
+        }
+        else if(ob.pr_mark[i]<60 && ob.pr_mark[i]>=55)
+        {
+            ob.pr_grade[i]="CC";
+            grade_point[i+3]=6;
+        }
+        else if(ob.pr_mark[i]<55 && ob.pr_mark[i]>=50)
+        {
+            ob.pr_grade[i]="CD";
+            grade_point[i+3]=5;
+        }
+        else if(ob.pr_mark[i]<50 && ob.pr_mark[i]>=45)
+        {
+            ob.pr_grade[i]="DD";
+            grade_point[i+3]=4;
+        }
+        else
+        {
+            ob.th_grade[i]="FF";
+            grade_point[i+3]=0;
+        }
+        mul_cr_gd+=(grade_point[i+3]*ob.subject_credit[i]);
     }
     ob.sgpa=(mul_cr_gd)/(ob.subject_credit[0] + ob.subject_credit[1] + ob.subject_credit[2]);
     cout<<endl<<setw(36)<<"Theory"<<setw(14)<<"Practical";
