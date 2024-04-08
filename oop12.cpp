4ae2d56fba78ca941a547d83d9bd780e10a3c020
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-08 12:43:32 +0530

    aa

--- oop12.cpp
+++ oop12.cpp
@@ -3,7 +3,30 @@
 #include<iostream>
 using namespace std;
 int main();
-class A;
+class Result;
+class Student
+{
+    string stu_id, stu_name;
+    int sem;
+    public:
+    void getdata_stu();
+    void putdata_stu();
+    void getdata_result(Result);
+    void putdata_result(Result);
+    float cal_sgpa(Student &S);
+};
+class Result
+{
+    int subject_credit, th_mark, pr_mark;
+    string subject_naame, th_grade, pr_grade;
+    float sgpa;
+    public:
+    friend float Student:: cal_sgpa(Student &s);
+};
+void Student :: getdata_stu()
+{
+
+}
 int main()
 {
     return 0;
