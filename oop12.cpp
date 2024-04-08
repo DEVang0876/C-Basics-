a9dcc866be99dc4ccce5f61b4e95b84aa99cea3a
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-08 17:59:33 +0530

    12 done

--- oop12.cpp
+++ oop12.cpp
@@ -57,21 +57,65 @@ void Student:: getdata_result(Result &ob)
 }
 void Student :: putdata_result(Result &ob)
 {
+    int grade_point[3],mul_cr_gd=0;
+
+    for(int i=0; i<3; i++)
+    {
+        if(ob.th_mark[i]>=80)
+        {
+           ob.th_grade[i]="AA";
+           grade_point[i]=10;
+        }
+        else if(ob.th_mark[i]<80 && ob.th_mark[i]>=73)
+        {
+            ob.th_grade[i]="AB";
+            grade_point[i]=9;
+        }
+        else if(ob.th_mark[i]<73 && ob.th_mark[i]>=66)
+        {
+            ob.th_grade[i]="BB";
+            grade_point[i]=8;
+        }
+        else if(ob.th_mark[i]<66 && ob.th_mark[i]>=60)
+        {
+            ob.th_grade[i]="BC";
+            grade_point[i]=7;
+        }
+        else if(ob.th_mark[i]<60 && ob.th_mark[i]>=55)
+        {
+            ob.th_grade[i]="CC";
+            grade_point[i]=6;
+        }
+        else if(ob.th_mark[i]<55 && ob.th_mark[i]>=50)
+        {
+            ob.th_grade[i]="CD";
+            grade_point[i]=5;
+        }
+        else if(ob.th_mark[i]<50 && ob.th_mark[i]>=45)
+        {
+            ob.th_grade[i]="DD";
+            grade_point[i]=4;
+        }
+        else
+        {
+            ob.th_grade[i]="FF";
+            grade_point[i]=0;
+        }
+        mul_cr_gd+=(grade_point[i]*ob.subject_credit[i]);
+    }
+    ob.sgpa=(mul_cr_gd)/(ob.subject_credit[0] + ob.subject_credit[1] + ob.subject_credit[2]);
     cout<<endl<<setw(36)<<"Theory"<<setw(14)<<"Practical";
-    cout<<endl<<setw(28)<<ob.subject_name[1]; cout<<setw(8)<<ob.th_grade[1];cout<<setw(12)<<ob.pr_grade[1];
-    cout<<endl<<setw(28)<<ob.subject_name[2]; cout<<setw(8)<<ob.th_grade[2];cout<<setw(12)<<ob.pr_grade[2];
-    cout<<endl<<setw(28)<<ob.subject_name[3]; cout<<setw(8)<<ob.th_grade[3];cout<<setw(12)<<ob.pr_grade[3];
-    
-
-
-
-
-   
-    cout<<endl<<endl<<ob.sgpa;
-    
-    
+    cout<<endl<<setw(28)<<ob.subject_name[0]; cout<<setw(8)<<ob.th_grade[1];cout<<setw(12)<<ob.pr_grade[0];
+    cout<<endl<<setw(28)<<ob.subject_name[1]; cout<<setw(8)<<ob.th_grade[2];cout<<setw(12)<<ob.pr_grade[1];
+    cout<<endl<<setw(28)<<ob.subject_name[2]; cout<<setw(8)<<ob.th_grade[3];cout<<setw(12)<<ob.pr_grade[2];
+    cout<<endl<<endl<<"SGPA = "<<ob.sgpa;
 }
 int main()
 {
+    Student s1; Result r1;
+    s1.getdata_stu();
+    s1.getdata_result(r1);
+    s1.putdata_stu();
+    s1.putdata_result(r1);
     return 0;
 }
