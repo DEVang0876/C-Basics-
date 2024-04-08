3be0b11a22f37fd36587f7d06a322ab68ed811eb
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-08 17:28:42 +0530

    12

---
+++ manip.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    cout<<setw(10)<<"Devang";
+    return 0;
+}
--- oop12.cpp
+++ oop12.cpp
@@ -1,28 +1,30 @@
 //23AIML014_DEVANG_DHANDHUKIYA
 //OOPs ex12
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main();
-class Student;
+class Result;
 class Student
 {
+
     char stu_id[7]; string stu_name;
     int sem;
     public:
     void getdata_stu();
     void putdata_stu();
-    void getdata_result(Result);
-    void putdata_result(Result);
+    void getdata_result(Result&);
+    void putdata_result(Result&);
+    friend class Result;
 };
 class Result
 {
-    int subject_credit, th_mark, pr_mark;
-    string th_grade, pr_grade;
-    char subject_name[3];
+    int subject_credit[3], th_mark[3], pr_mark[3];
+    string th_grade[3], pr_grade[3];
+    string subject_name[3];
     float sgpa;
     public:
-     friend void  Student :: getdata_result(Result);
-     friend void  Student :: putdata_result(Result);
+    friend class Student;
 };
 void Student :: getdata_stu()
 {
@@ -39,20 +41,34 @@ void Student:: putdata_stu()
     cout<<endl<<"Student name    :  "; cout<<stu_name;
     cout<<endl<<"Semester        :  "; cout<<sem;
 }
-void Student:: getdata_result(Result)
+void Student:: getdata_result(Result &ob)
 {
-    cout<<endl<<"Enter Subject Name: ";
-    cout<<endl<<"Enter Subject Credit";
-    cin>>subject_credit;
-    cin.getline(subject_name,3);
+    for(int i=0; i<3; i++)
+    {
+    cout<<endl<<"Enter Subject "<<i<<" Name: ";
+    cin>>ob.subject_name[i];
+    cout<<endl<<"Enter Subject "<<i<<" Credit";
+    cin>>ob.subject_credit[i];
     cout<<endl<<"Enter Theory Marks: ";
-    cin>>th_mark;
+    cin>>ob.th_mark[i];
     cout<<endl<<"Enter Practical Marks: ";
-    cin>>pr_mark;
+    cin>>ob.pr_mark[i];
+    }
 }
-void Student :: putdata_result(Result)
+void Student :: putdata_result(Result &ob)
 {
-    cout<<endl<<subject_name; cout<<"         "<<th_grade;cout<<"        "<<pr_grade;
+    cout<<endl<<setw(36)<<"Theory"<<setw(14)<<"Practical";
+    cout<<endl<<setw(28)<<ob.subject_name[1]; cout<<setw(8)<<ob.th_grade[1];cout<<setw(12)<<ob.pr_grade[1];
+    cout<<endl<<setw(28)<<ob.subject_name[2]; cout<<setw(8)<<ob.th_grade[2];cout<<setw(12)<<ob.pr_grade[2];
+    cout<<endl<<setw(28)<<ob.subject_name[3]; cout<<setw(8)<<ob.th_grade[3];cout<<setw(12)<<ob.pr_grade[3];
+    
+
+
+
+
+   
+    cout<<endl<<endl<<ob.sgpa;
+    
     
 }
 int main()
