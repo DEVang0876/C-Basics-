eb165ab357a7758e9c4505bd845040a7ae6d7f37
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-15 13:00:50 +0530

    praac 14

---
+++ oop13.cpp
@@ -0,0 +1,22 @@
+//DEVANG_DHANDHUKIYA_23AIML014
+//OOPc 12
+
+#include<iostream>
+using namespace std;
+int main();
+class library
+{
+
+};
+class book: public virtual library
+{
+
+};
+class magezine: public virtual library
+{
+
+};
+class e_resources: public book, public magezine
+{
+
+};
