31ef47c6f25a9bacb7eca7288e909a7e9a4e402f
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-19 23:03:22 +0530

    done 13

--- oop13.cpp
+++ oop13.cpp
@@ -6,17 +6,162 @@ using namespace std;
 int main();
 class library
 {
-
+    string title;
+    int id;
+    public:
+    void add_title();
+    void put_id_tile(){cout<<endl<<id<<"    "<<title;}
 };
+void library:: add_title()
+{
+    cout<<endl<<"ID  : ";
+    cin>>id; 
+    cout<<endl<<"Title  : ";
+    cin>>title;
+}
 class book: public virtual library
 {
-
+    int isbn;
+    int copy;
+    public:
+    void add_book_isbn();
+    void add_book_stock();
+    void put_book_isbn_copy(){cout<<isbn<<"    "<<"copies"<<copy;};
 };
+void book:: add_book_isbn()
+{
+    cout<<endl<<"ISBN  : ";
+    cin>>isbn;
+}
+void book:: add_book_stock()
+{
+    cout<<"In Stock copies  : ";
+    cin>>copy;
+}
 class magezine: public virtual library
 {
-
+    int issn;
+    int cp;
+    public:
+    void  add_issn();
+    void add_mag_stock();
 };
+void magezine:: add_issn()
+{
+    cout<<endl<<"ISSN  : ";
+    cin>>issn;
+}
+void magezine:: add_mag_stock()
+{
+    cout<<endl<<"In stock copies  : ";
+}
 class e_resources: public book, public magezine
 {
-
+    string doi;
+    static int count_book;
+    static int count_mag;
+    public:
+    void add_book(e_resources &);
+    void add_magezine(e_resources &);
+    void list_book(e_resources &);
+    void list_mag(e_resources &);
+    static int get_count_book(){return count_book;}
+    static int get_count_mag(){return count_mag;}
 };
+int e_resources:: count_book=0;
+int e_resources:: count_mag=0;
+
+void e_resources:: list_book(e_resources &b)
+{
+    b.put_id_tile();cout<<"    ";b.put_book_isbn_copy();
+ 
+}
+void e_resources:: list_mag(e_resources &l)
+{
+
+}
+
+void e_resources:: add_book(e_resources &b)
+{
+    b.add_title();
+    b.add_book_isbn();
+    cout<<"(E)copy or (H)ardbound  : ";
+    char ch;
+    cin>>ch;
+    if(ch=='H')
+    {
+        b.add_book_stock();
+    }
+    else if(ch=='E')
+    {
+        cout<<endl<<"DOI  : ";
+        cin>>doi;
+    }
+    count_book++;
+}
+void e_resources:: add_magezine(e_resources &m)
+{
+    m.add_title();
+    m.add_issn();
+    cout<<"(E)copy or (H)ardbound  : ";
+    char ch;
+    cin>>ch;
+    if(ch=='H')
+    {
+        m.add_mag_stock();
+    }
+    else if(ch=='E')
+    {
+        cout<<endl<<"DOI  : ";
+        cin>>doi;
+    }
+    count_mag++;
+} 
+int main()
+{
+    library l_m[50];
+    magezine m[50];
+    e_resources ebook[50], emag[50];
+    main_menu:
+    cout<<endl<<"Enter 1 : Add book data";
+     cout<<endl<<"Enter 2 : Add magezine data";
+     cout<<endl<<"Enter 3 : List all books";
+     cout<<endl<<"Enter 4 : List all magezines";
+     cout<<endl<<"Enter your choice  : ";
+     int n;
+     cin>>n;
+     int mag=e_resources::get_count_mag();
+     int book=e_resources::get_count_book();
+    switch (n)
+    {
+    case 1:
+        cout<<endl<<"**** Add book data ****";
+        ebook[book].add_book(ebook[book]);
+        break;
+    case 2:
+        cout<<endl<<"**** Add maagezine data ****";
+        emag[mag].add_magezine(emag[mag]);
+        break;
+    case 3:
+        cout<<endl<<"**** List all books";
+        for(int x=0; x<book; x++)
+        {
+           ebook[x].list_book(ebook[x]);
+        }
+        break;
+    case 4:
+        cout<<endl<<"**** List all magezines";
+        for (int y=0; y<mag; y++)
+        {
+            emag[y].list_mag(emag[mag]);
+        }
+    default:
+        break;
+    }
+    cout<<endl<<"----------------------------------------------------------------";
+    cout<<endl<<"Enter M to goto mian menu, Press any key to exit";
+    char chx;
+    cin>>chx;
+    if(chx=='M'){goto main_menu;}
+    return 0;
+}
