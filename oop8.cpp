88bfd274fc1f6f3131a07e1b2298327b1e36cc78
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-25 19:32:54 +0530

    aaa

--- oop8.cpp
+++ oop8.cpp
@@ -7,16 +7,30 @@ class employe
     string name, id, qual;
     float ex, avgx;
     long int cont;
+   
 
-    protected:
+    public:
     void addinfo();
     void printinfo();
     int findID(string str);
 
     public:
     float avgexp();
+    static int count;
 
 };
+int employe:: count=0;
+int employe:: findID(string str)
+{   
+if(str==id)
+{
+return 1;
+}       
+else
+{
+return 0;
+}
+}
 void employe:: addinfo()
 {
         cout<<"Eneter Employee ID : ";
@@ -30,7 +44,7 @@ void employe:: addinfo()
         cin>>ex;
         cout<<"Eneter Employee Contact Number : ";
         cin>>cont;
-        
+        count++;
 }
 void employe:: printinfo()
 {
@@ -41,15 +55,37 @@ void employe:: printinfo()
         cout<<"Contact Number :  "<<cont<<endl;
         cout<<"-------------------------------------"<<endl;
 }
+void printerror()
+{
+        cout<<endl<<"*************************";
+        cout<<endl<<"ERROR: ID MATCH NOT FOUND";
+        cout<<endl<<"*************************";
+}
 class tcEmp: public employe
 {
-    string dsg, spzn, paysl;
+    char dsg[40], spzn[40], paysl[20];
 
     public:
     void searchtemp();
     void gettempdata();
     void puttempdata();
 };
+void tcEmp:: gettempdata()
+{
+    cout<<"Enter Designation : ";
+    cin.getline(dsg, 40);
+    cout<<endl<<"Enter Specilization : ";
+    cin.getline(spzn, 40);
+    cout<<endl<<"Enter Pay Scale : ";
+    cin.getline(paysl, 20);
+
+}
+void tcEmp:: puttempdata()
+{
+    cout<<endl<<"Designation    :"<<dsg;
+    cout<<endl<<"Spcialization  :"<<spzn;
+    cout<<endl<<"Pay Scale      :"<<paysl;
+}
 class ntEmp: public employe
 {
     int salary;
@@ -60,7 +96,30 @@ class ntEmp: public employe
     void putNtempdata();
 
 };
+void ntEmp:: getNtempdata()
+{
+    cout<<endl<<"Enter Salary : ";
+    cin>>salary;
+
+}
+void ntEmp:: putNtempdata()
+{
+    cout<<"salary         :"<<salary;
+}
 int main()
 {
+    employe obj[1000];
+    tcEmp tcobj[500];
+    ntEmp ntcobj[500];
+    int ne,fid;
+    cout<<endl<<"Enter Number of employs: ";
+    cin>>ne;
+    for (int i=0;i<ne;i++)
+    {
+        cout<<"Enter data for employe "<<(i+1)<<endl;
+        obj[i].addinfo();
+    }
+    
+    
     return 0;
 }
