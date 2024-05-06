ed08600d255c2bc9c2040a16cddd971e7928b794
DEVang0876 <23aiml014@charusat.edu.in>
2024-05-06 21:18:32 +0530

    dd

---
+++ ttt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a; long b; char ch; float x; double y;
+    cin>>a; cin>>b; cin>>ch; cin>>x; cin>>y;
+    cout<<a;
+    cout<<endl<<b;
+    cout<<endl<<ch;
+    cout<<endl<<x;
+    cout<<endl<<y;
+    return 0;
+}
