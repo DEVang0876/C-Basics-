123532899cab37be9dadea59defc6d5288b3d0e4
DEVang0876 <23aiml014@charusat.edu.in>
2024-05-11 15:04:08 +0530

    fffff

--- oop16.cpp
+++ oop16.cpp
@@ -1,4 +1,16 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
-int main();
+int main()
+{
+char line[50];
+ofstream cars;
+cars.open("cardata.txt");
+cars.close();
 
+ifstream f;
+f.open("cardata.txt");
+f.getline(line,50);
+cout<<endl<<line;
+f.close();
+}
