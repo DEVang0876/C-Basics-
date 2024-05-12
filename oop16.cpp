2917945d87bab94313ddd7ed438e0f01802fcd44
DEVang0876 <23aiml014@charusat.edu.in>
2024-05-12 11:17:42 +0530

    g

--- oop16.cpp
+++ oop16.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-char line[50];
+    char line[50];
 ofstream cars;
 cars.open("cardata.txt");
 cars.close();
@@ -13,4 +13,26 @@ f.open("cardata.txt");
 f.getline(line,50);
 cout<<endl<<line;
 f.close();
+    cout<<endl<<"  WELCOME TO TATA MOTORS  ";
+    menu:
+    cout<<endl<<"GET THE CAR DETAILS AS PER YPUR PREFERENCES";
+    cout<<"(1) Model name  (2) Fiel type  (3) Prce range";
+    cout<<endl<<"ENTER YOUR OPTION : ";
+    cout<<endl<<"_____________________________________________";
+    int choice;
+    cin>>choice;
+    switch (choice)
+    {
+    case 1:
+    
+        
+        break;
+    
+    default:
+        cout<<endl<<"ENTER VALID OPTION !!!!";
+        goto menu;
+        break;
+    }
+ 
+
 }
